5d92993cdc6d8d82de102355a447aa4e57f8b842
laurabaldryan <63228952+laurabaldrayan@users.noreply.github.com>
2025-04-13 20:22:05 +0400

    Initial commit: Nuts Service Subscriber

---
+++ NutsServiceSubscriber/src/main/java/org/example/nutssubscriber/NutsSubscriberApplication.java
@@ -0,0 +1,12 @@
+package org.example.nutssubscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutsSubscriberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutsSubscriberApplication.class, args);
+    }
+}
